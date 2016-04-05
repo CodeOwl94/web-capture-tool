e19f667389961802be16a773cb4e8937177524de
CodeOwl94 <ashwin@bloom.org.au>
2016-04-05 11:58:25 +0800

    Made some changes to the js and updated the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# web-capture-tool
-A basic web capture tool for experiments run by the System Health Lab, UWA
+
+
+
+#Web Capture Tool 
+---------------------------
+This web capture tool was built to assist experimenters at the System Health Lab, University of Western Australia record data. However, the tool was also built with the intention that the general public can use it for their needs as well. As such, it is well documented and all the source code written is available. 
+
+##Installation
+-------------------------
+The source code for this tool is a simple cobbling together of HTML, PHP and jQuery scripts. Bootstrap was used for formatting purposes however it is linked to using [CDN](http://getbootstrap.com/getting-started/) so you will not need to download the Bootstrap source code. Furthermore, the jQuery library is simply the bundle provided by [Google](https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js).
+
+
+##Example View
+------------------------
+Stay tuned while an example site is uploaded to view. 
+
+ 
+
+
--- webCaptureTool/scripts/script.js
+++ webCaptureTool/scripts/script.js
@@ -19,184 +19,123 @@ $(document).ready(function(){
 
 	});
 
-	/*Not being used anymore
-	//Populate the date
-	var dNow = new Date();
-	var utcdate= dNow.getDate() + '/' + (dNow.getMonth()+ 1) + '/' +  dNow.getFullYear();
-	$('#date').text(utcdate)*/
+	//Check the form inputs
+	/*
+	function check_inputs(){
 
-	//Check the form
+		var failure = 0;
+
+		if (!($('#start_date').val())) {
+			alert("Please choose a start date");
+			failure = 1;
+		}
+
+		if (!($('#start_time').val())) {
+			alert("Please choose a start time");
+			failure = 1;
+		}
+		else{ //Format the time a little
+			date = ($('#start_date').val());
+			time = ($('#start_time').val());
+			
+			placeHolder = date + " " + time + ":00";
+
+			$('<input>', {
+			    type: 'hidden',
+			    id: 'start_time_proper',
+			    name: 'start_time_proper',
+			    value: placeHolder
+			}).appendTo('form');
+
+		}
+
+
+		if (!($('#end_date').val())) {
+			alert("Please choose a end date");
+			failure = 1;
+		}
+
+		if (!($('#end_time').val())) {
+			alert("Please choose a end time");
+			failure = 1;
+		}
+		else{ //Format the time a little
+			date = ($('#end_date').val());
+			time = ($('#end_time').val());
+
+			placeHolder = date + " " + time + ":00";
+
+			$('<input>', {
+			    type: 'hidden',
+			    id: 'end_time_proper',
+			    name: 'end_time_proper',
+			    value: placeHolder
+			}).appendTo('form');
+ 
+		}
+
+		if (!($('#motor').val())) { //does the value exist?
+			alert("Please choose a motor number");
+			failure = 1;
+		}
+		else{ //check that value makes sense
+
+			var value = +($('#motor').val()); //the + converts it to an int
+
+			
+			failure = 1;
+
+		}
+
+		if (!($('#tyre_ring').val())) {
+			alert("Please choose a tyre ring set number");
+			failure = 1;
+		}
+
+		if (!($('#drive_pulley').val())) {
+			alert("Please choose a drive pulley number");
+			failure = 1;
+		}
+
+		if (!($('#drive_belt').val())) {
+			alert("Please choose a drive belt number");
+			failure = 1;
+		}
+
+		if (!($('#comments').val())) {
+			alert("Please add a comment");
+			failure = 1;
+		}
+
+		if (!($('#fileToUpload').val())) {
+			alert("Please add a file");
+			failure = 1;
+		}
+
+		return failure;
+
+	}
+
+	//Was either submit button pressed? 
 	$('#submit_begin').click(function(){
-
 		
-		if (!($('#start_time').val())) {
-			alert("Please choose a start time");
-			return false;
+		var failure = check_inputs();
+		if (failure) {
+
+			alert("triggered");
 		}
 		
-		if (!($('#start_date').val())) {
-			alert("Please choose a start date");
-			return false;
-		}
-		else{ //Format the time a little
-			date = ($('#start_date').val());
-			time = ($('#start_time').val());
-			alert("Here");
-
-			placeHolder = date + " " + time + ":00";
-
-			$('<input>', {
-			    type: 'hidden',
-			    id: 'start_time_proper',
-			    name: 'start_time_proper',
-			    value: placeHolder
-			}).appendTo('form');
-
-			alert(placeHolder); 
-		}
-
-
-		if (!($('#end_time').val())) {
-			alert("Please choose a end time");
-			return false;
-		}
-
-		if (!($('#end_date').val())) {
-			alert("Please choose a end date");
-			return false;
-		}
-		else{ //Format the time a little
-			date = ($('#end_date').val());
-			time = ($('#end_time').val());
-
-			placeHolder = date + " " + time + ":00";
-
-			$('<input>', {
-			    type: 'hidden',
-			    id: 'end_time_proper',
-			    name: 'end_time_proper',
-			    value: placeHolder
-			}).appendTo('form');
-
-			alert(placeHolder); 
-		}
-
-		if (!($('#motor').val())) { //add some more checking to make sure number if positive integer later 
-			alert("Please choose a motor number");
-			return false;
-		}
-
-		if (!($('#tyre_ring').val())) {
-			alert("Please choose a tyre ring set number");
-			return false;
-		}
-
-		if (!($('#drive_pulley').val())) {
-			alert("Please choose a drive pulley number");
-			return false;
-		}
-
-		if (!($('#drive_belt').val())) {
-			alert("Please choose a drive belt number");
-			return false;
-		}
-
-		if (!($('#comments').val())) {
-			alert("Please add a comment");
-			return false;
-		}
-
-		if (!($('#fileToUpload').val())) {
-			alert("Please add a file");
-			return false;
-		}
 	});
 
-$('#submit_end').click(function(){
+	$('#submit_end').click(function(){
 
-		
-		if (!($('#start_time').val())) {
-			alert("Please choose a start time");
-			return false;
-		}
-		
-		if (!($('#start_date').val())) {
-			alert("Please choose a start date");
-			return false;
-		}
-		else{ //Format the time a little
-			date = ($('#start_date').val());
-			time = ($('#start_time').val());
-			alert("Here");
+			var failure = check_inputs();
+			if (failure) {
 
-			placeHolder = date + " " + time + ":00";
-
-			$('<input>', {
-			    type: 'hidden',
-			    id: 'start_time_proper',
-			    name: 'start_time_proper',
-			    value: placeHolder
-			}).appendTo('form');
-
-			alert(placeHolder); 
-		}
-
-
-		if (!($('#end_time').val())) {
-			alert("Please choose a end time");
-			return false;
-		}
-
-		if (!($('#end_date').val())) {
-			alert("Please choose a end date");
-			return false;
-		}
-		else{ //Format the time a little
-			date = ($('#end_date').val());
-			time = ($('#end_time').val());
-
-			placeHolder = date + " " + time + ":00";
-
-			$('<input>', {
-			    type: 'hidden',
-			    id: 'end_time_proper',
-			    name: 'end_time_proper',
-			    value: placeHolder
-			}).appendTo('form');
-
-			alert(placeHolder); 
-		}
-
-		if (!($('#motor').val())) { //add some more checking to make sure number if positive integer later 
-			alert("Please choose a motor number");
-			return false;
-		}
-
-		if (!($('#tyre_ring').val())) {
-			alert("Please choose a tyre ring set number");
-			return false;
-		}
-
-		if (!($('#drive_pulley').val())) {
-			alert("Please choose a drive pulley number");
-			return false;
-		}
-
-		if (!($('#drive_belt').val())) {
-			alert("Please choose a drive belt number");
-			return false;
-		}
-
-		if (!($('#comments').val())) {
-			alert("Please add a comment");
-			return false;
-		}
-
-		if (!($('#fileToUpload').val())) {
-			alert("Please add a file");
-			return false;
-		}
+				return false;
+			}	
 	});
+	*/
 
 
 	
